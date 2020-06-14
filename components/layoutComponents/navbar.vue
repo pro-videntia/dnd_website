0820bf8eae712648fabc34cc457f257350a24cb0
Tyler Boyd <tylerb@theboydprojects.com>
2020-06-13 19:21:07 -0500

    added layoutcomponents folder, added navbar component, layed out basic structure of navbar

---
+++ components/layoutComponents/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app-bar>
+        <v-toolbar-title>Dungeons and Dating</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text rounded to="/">
+            Home
+        </v-btn>
+        <v-btn text rounded to="/login">
+            Login
+        </v-btn>
+        <v-btn text rounded to="/signup">
+            Sign Up
+        </v-btn>
+        
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
