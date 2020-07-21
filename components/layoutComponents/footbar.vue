0fab6dfae1e45661a46e631148bff3e36d39db74
Tyler Boyd <tylerb@theboydprojects.com>
2020-07-20 19:16:27 -0500

    added footer, which matches mock up styles

---
+++ components/layoutComponents/footbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-footer :absolute="!fixed" class="py-10" app>
+      
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false
+    };
+  }
+};
+</script>
+
+<style scoped></style>
